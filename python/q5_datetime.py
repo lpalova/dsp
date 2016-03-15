171a8bd4405f51eedb909eead197b77d1bb626d8
lpalova <lpalova@users.noreply.github.com>
2016-03-15 16:57:59 -0400

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -10,8 +10,13 @@ diff = date_stop_object - date_start_object
 diff.days
 
 ####b)  
+from datetime import datetime  
 date_start = '12312013'  
-date_stop = '05282015'  
+date_stop = '05282015' 
+date_start_object = datetime.strptime(date_start, '%m%d%Y')    
+date_stop_object = datetime.strptime(date_stop, '%m%d%Y')    
+diff = date_stop_object - date_start_object    
+diff.days 
 
 ####c)  
 date_start = '15-Jan-1994'  
