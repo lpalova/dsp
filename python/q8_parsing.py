5e6cd6bda1cf3c2f251029676deff834760d0543
lpalova <lpalova@users.noreply.github.com>
2016-03-16 13:47:32 -0400

    Update q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -6,14 +6,32 @@
 
 # The below skeleton is optional.  You can use it or you can write the script with an approach of your choice.
 
-
 import csv
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
+def read_data(data):
+    list = []
+    with open(data, 'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            list.append(row)
+    return list
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+def get_min_score_difference(list):
+#(self, parsed_data):
+    imin = 0
+    min = abs( int(list[0]['Goals']) - int(list[0]['Goals Allowed']) )
+    for i in range(1, len(list)):
+        diff = abs( int(list[i]['Goals']) - int(list[i]['Goals Allowed']) )
+        if diff < min:
+            imin = i
+            min = diff
+    return imin
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+def get_team (list, imin):
+#(self, index_value, parsed_data):
+    return list[imin]['Team']  
+
+list = read_data( 'football.csv' )
+imin = get_min_score_difference( list )
+team = get_team( list, imin )
+print team
