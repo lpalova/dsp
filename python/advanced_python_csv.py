0fe6199626f4a914b0f9778071922d9d327cc39f
lpalova <lpalova@users.noreply.github.com>
2016-03-17 09:53:05 -0400

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,30 @@
-PLACE YOUR CODE HERE
+import csv
+import re
+
+def read_data(myfile):
+    dict_list = []
+    with open(myfile, 'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            dict_list.append(row)
+    return dict_list
+
+def get_list(dict_list, name):
+    my_list = []
+    for d in dict_list:
+        my_list.append(d[name])
+    return my_list
+
+def write_data(mylist, myfile):
+    with open(myfile, 'wb') as csvfile:
+#       writer = csv.writer(csvfile)
+        for item in mylist:
+            csvfile.write("%s\n" % item)
+#        Example with writerow(s)
+#        writer.writerow(["Name","Address","Telephone","Fax","E-mail","Others"])
+#        writer.writerows(mylist) -- separates each char with , ?
+
+# Q5. Write email addresses from Part I to csv file
+dict_list = read_data( 'faculty.csv' )
+email_list = get_list(dict_list, ' email')
+write_data(email_list, 'emails.csv')
